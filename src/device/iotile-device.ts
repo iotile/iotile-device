49bc1da3a95b2f800cfceccbf5c7da75decb138b
Kaylie DeHart <kaylie.dehart@gmail.com>
2018-11-06 11:01:17 -0800

    RTC Support (#21)

--- src/device/iotile-device.ts
+++ src/device/iotile-device.ts
@@ -56,6 +56,20 @@ export interface DeviceInfo {
   osVersion: string
 }
 
+export interface DeviceUptime {
+  isUTC: false;
+  isSynchronized: false;
+  currentTime: number;
+}
+
+export interface DeviceUTCTime {
+  isUTC: true;
+  isSynchronized: boolean;
+  currentTime: Date;
+}
+
+export type DeviceTime = DeviceUptime | DeviceUTCTime;
+
 export interface ReceiveReportsOptions {
   expectedStreamers: {[key: number]: string},   //The indices of the streamers that we expect to receive reports from as well as the names that should be used in the ProgressNotifier
   requireAll?: boolean,           //Whether an exception should be received if there is no data on one or more of the streamers
@@ -556,6 +570,53 @@ export class IOTileDevice {
 
   public config(): Config {
     return new Config(this.adapter);
+  }
+
+  public async currentTime(synchronizationSlopSeconds: number = 60): Promise<DeviceTime> {
+    let deviceTime: DeviceTime;
+    let [timestamp] = await this.adapter.typedRPC(8, 0x1001, "", "L", []);
+    catAdapter.info(`Timestamp is: ${timestamp}`);
+
+    if (!!(timestamp & (1 << 31)) === true){
+      let secondsSince2000 = timestamp & ((1 << 31) - 1);
+      catAdapter.info(`Seconds since 2000: ${secondsSince2000}`);
+      let convertedSeconds = (Date.UTC(2000, 0, 1) / 1000) + secondsSince2000;
+      let convertedTime = new Date(convertedSeconds * 1000);
+      let currentSeconds = Date.now() / 1000;
+      
+      //If UTC is set and decoded Date is within synchronizationSlopSeconds of our current time
+      //sets isSynchronized to true.
+      let synched = (Math.abs(convertedSeconds - currentSeconds) <= synchronizationSlopSeconds);
+
+      //Returns a DeviceUTCTime if UTC is set
+      deviceTime = {
+        isUTC: true,
+        isSynchronized: synched,
+        currentTime: convertedTime
+      };
+    } else {
+      //Returns a DeviceUptime if UTC is not set
+      deviceTime = {
+          isUTC: false,
+          isSynchronized: false,
+          currentTime: timestamp
+        };
+    }
+    
+    return deviceTime;
+  }
+
+  public async synchronizeTime(forcedTime?: Date): Promise<number> {
+    if (!forcedTime){
+      forcedTime = new Date();
+    }
+
+    let millisecondsAt2000 = Date.UTC(2000, 0, 1);
+    let secondsSince2000 = Math.ceil((forcedTime.valueOf() - millisecondsAt2000) /1000);
+    catAdapter.info(`Sending time to RTC: ${secondsSince2000}`);
+
+    await this.adapter.errorHandlingRPC(8, 0x1010, "L", "L", [secondsSince2000]);
+    return secondsSince2000;
   }
 
   public async downloadStream(streamName: string, progress?: any): Promise<RawReading[]> {
