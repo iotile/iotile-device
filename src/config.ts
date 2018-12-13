98dd76623f9bf09c1ecf3c1a7b1c8dec710f7122
Tim Burke <timburke@alumni.rice.edu>
2018-12-13 11:56:56 -0800

    Fix report id range calculation after reassembly.
    - Previously the lowest and highest ids were not properly updated
    - Adds test covergae that updating the lowest and highest IDs works
      correctly.

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,7 @@ CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLeve
 // Create categories, they will autoregister themselves, one category without parent (root) and a child category.
 export const catService = new Category("iotile.device");
 export const catAdapter = new Category("IOTileAdapter", catService);
+export const catReports = new Category("Reports", catService);
 export const catUTCAssigner = new Category('UTCAssigner', catService);
 export const catBLEOptimizer = new Category("BLEOptimizer", catService);
 export const catMockBLE = new Category("MockBLE", catService);
